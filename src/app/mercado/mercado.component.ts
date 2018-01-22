c37b30af96b73ab2c73a8a8938afbedc6645abac
Guilherme Rodrigues <guilherme.rodrigues@sabium.com.br>
2018-01-22 16:58:54 -0200

    preenchendo mercados

--- src/app/mercado/mercado.component.ts
+++ src/app/mercado/mercado.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {FormGroup, FormControl} from '@angular/forms'
 import { Validators } from '@angular/forms';
+import { Mercado } from '../models/mercado.model'
 
 @Component({
   selector: 'app-mercado',
@@ -9,13 +10,83 @@ import { Validators } from '@angular/forms';
 })
 export class MercadoComponent implements OnInit {
 
+  public mercados: Array<Mercado>
+
   constructor() { }
 
   ngOnInit() {
+    this.mercados = [
+      {
+        codigo: 1,
+        descricao: "Mercado América",
+        razaoSocial: "Mercado América LTDA",
+        nomeFantasia: "Mercado América",
+        cnpjCpf: "16308837000132",
+        inscricaoEstadual: "123456789",
+        endereco: "Av. Fulano de tal Numero 123",
+        telefone: "(44) 3641-2526",
+        celular: "(44)99985-6522"   
+      },
+      {
+        codigo: 2,
+        descricao: "Mercado Pinelli",
+        razaoSocial: "Mercado Pinelli LTDA",
+        nomeFantasia: "Mercado Pinelli",
+        cnpjCpf: "16308837000777",
+        inscricaoEstadual: "123456789",
+        endereco: "Av. Fulano de tal Numero 123",
+        telefone: "(44) 3641-2825",
+        celular: "(44)99985-3030"   
+      },
+      {
+        codigo: 3,
+        descricao: "Mercado Planalto",
+        razaoSocial: "Mercado Planalto LTDA",
+        nomeFantasia: "Mercado Planalto",
+        cnpjCpf: "16308837000888",
+        inscricaoEstadual: "123456789",
+        endereco: "Av. Fulano de tal Numero 123",
+        telefone: "(44) 3641-8745",
+        celular: "(44)99985-1020"   
+      },
+      {
+        codigo: 4,
+        descricao: "Mercado Bella Vista",
+        razaoSocial: "Mercado Bella Vista LTDA",
+        nomeFantasia: "Mercado Bella Vista",
+        cnpjCpf: "16308837000999",
+        inscricaoEstadual: "123456789",
+        endereco: "Av. Fulano de tal Numero 123",
+        telefone: "(44) 3641-7854",
+        celular: "(44)99985-1234"   
+      },
+
+    ]
   }
 
   public formulario: FormGroup = new FormGroup({
-    'email': new FormControl(null, [Validators.required, Validators.email]),
-    'senha': new FormControl(null, [Validators.required, Validators.minLength(6)])
+    'codigo': new FormControl(null),
+    'descricao': new FormControl(null),
+    'razaoSocial': new FormControl(null),
+    'nomeFantasia': new FormControl(null),
+    'cnpjCpf': new FormControl(null),
+    'inscricaoEstadual': new FormControl(null),
+    'endereco': new FormControl(null),
+    'telefone': new FormControl(null),
+    'celular': new FormControl(null)
   })
+
+  public editarMercado(mercado): void{
+    this.formulario.controls.codigo.setValue(mercado.codigo)
+    this.formulario.controls.descricao.setValue(mercado.descricao)    
+    this.formulario.controls.razaoSocial.setValue(mercado.razaoSocial) 
+    this.formulario.controls.nomeFantasia.setValue(mercado.nomeFantasia) 
+    this.formulario.controls.cnpjCpf.setValue(mercado.cnpjCpf) 
+    this.formulario.controls.inscricaoEstadual.setValue(mercado.inscricaoEstadual) 
+    this.formulario.controls.endereco.setValue(mercado.endereco) 
+    this.formulario.controls.telefone.setValue(mercado.telefone) 
+    this.formulario.controls.celular.setValue(mercado.celular) 
+    
+   console.log(this.formulario.value)
+  }
 }
---
+++ src/app/models/mercado.model.ts
@@ -0,0 +1,13 @@
+export class Mercado{
+    constructor(
+        public codigo: number,
+        public descricao: string,
+        public razaoSocial: string,
+        public nomeFantasia: string,
+        public cnpjCpf: string,
+        public inscricaoEstadual: string,
+        public endereco: string,
+        public telefone: string,
+        public celular: string
+    ){}
+}
