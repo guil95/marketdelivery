c7b213da555f77fc260dc477316279985f1be7bc
Guilherme Rodrigues <guilherme.rodrigues@sabium.com.br>
2018-02-19 08:14:49 -0300

    not found

--- src/app/admin/home/home.component.ts
+++ src/app/admin/home/home.component.ts
@@ -1,3 +1,6 @@
+import { Mercado } from './../../models/mercado.model';
+import { URL_API_MERCADOS } from './../../app.api';
+import { CrudService } from './../../services/crud.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -6,10 +9,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-
-  constructor() { }
+  public mercados
+  constructor(private crudService: CrudService) { }
 
   ngOnInit() {
+    this.buscarMercados()
+  }
+
+  public buscarMercados(){
+    this.crudService.get<Mercado[]>(URL_API_MERCADOS).subscribe(ret => {
+     
+      this.mercados = ret['data'].length;
+    }, (err) => {
+      console.log(err)
+    })
   }
 
 }
