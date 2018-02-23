602a6e5ee68b91efdd6fbb277e649ee65d4c9f4c
Guilherme Rodrigues <guilherme.rodrigues@sabium.com.br>
2018-02-23 12:42:12 -0300

    #

--- src/app/app.routing.module.ts
+++ src/app/app.routing.module.ts
@@ -1,7 +1,7 @@
 import { AdminComponent } from './admin/admin.component';
 import { NgModule } from '@angular/core';
 
-import { Routes, RouterModule } from '@angular/router'
+import { Routes, RouterModule, PreloadAllModules } from '@angular/router'
 import { LoginComponent } from './login/login.component'
 import { AuthGuard } from './guards/auth.guard';
 
