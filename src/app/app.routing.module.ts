68d47460fa0a7be77c8aa90ea191c006aedc9446
Guilherme Rodrigues <guilhermerodriguestb@gmail.com>
2018-03-05 22:30:53 -0300

    demonios

--- src/app/app.routing.module.ts
+++ src/app/app.routing.module.ts
@@ -4,6 +4,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router'
 import { LoginComponent } from './login/login.component'
 import { AuthGuard } from './guards/auth.guard';
+import { PreloadAllModules } from '@angular/router';
 
 export const ROUTES: Routes = [
     {
